10404a51147353adacb8c1e145def7c3fcfb161c
kajikaji5678 <bestlife.fukuda.kajito@gmail.com>
2026-07-22 21:15:25 +0900

    APIチェック

--- src/App.tsx
+++ src/App.tsx
@@ -64,7 +64,7 @@ export default function App() {
 
   return (
     <>
-    <div className="fixed top-0 left-0 z-50 bg-black text-white p-4">
+    <div className="text-4xl">
       TEST
     </div>
       <BrowserRouter>
