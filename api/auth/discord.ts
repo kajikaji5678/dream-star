88ec58a406d3510534e9f22ce47bb86fc61a1ed9
kajikaji5678 <bestlife.fukuda.kajito@gmail.com>
2026-07-23 20:27:05 +0900

    env変更

--- api/auth/discord.ts
+++ api/auth/discord.ts
@@ -1,7 +1,7 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
 
 
-export default function handler(
+export default async function handler(
   req: VercelRequest,
   res: VercelResponse
 ) {
@@ -11,16 +11,32 @@ export default function handler(
     });
   }
 
-  const {code} = req.body;
+  const { code } = req.body;
 
   if(!code) {
     return res.status(400).json({
       message: "Code is required",
-    })
+    });
   }
 
-  return res.status(200).json({
-    message: "recived",
-    code
-  })
+  const tokenResponse = await fetch("https://discord.com/api/oauth2/token", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: new URLSearchParams({
+      client_id: process.env.DISCORD_CLIENT_ID!,
+      client_secret: process.env.DISCORD_CLIENT_SECRET!,
+      grant_type: "authorization_code",
+      code
+    }),
+  });
+
+  const tokenData = await tokenResponse.json();
+
+  if (!tokenResponse.ok) {
+    return res.status(400).json(tokenData);
+  }
+
+  return res.status(200).json(tokenData);
 }
