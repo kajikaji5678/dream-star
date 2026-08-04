307df84cd3a8dfae9649dcf2e1eabda6014dd484
kajikaji5678 <bestlife.fukuda.kajito@gmail.com>
2026-08-04 18:07:08 +0900

    サービスにてDB登録処理追加

--- server/routes/gacha.ts
+++ server/routes/gacha.ts
@@ -1,12 +1,15 @@
 import { Router } from "express";
 import { drawCard } from "../service/gachaService.ts";
+import { saveUserCard } from "../service/gachaService.ts";
 
 const router = Router();
 
 router.post("/", async(req, res) => {
   try {
+    const {userId} = req.body;
     const card = await drawCard();
-    res.json({card, body: req.body});
+    await saveUserCard(userId, card.id);
+    res.json(card);
   } catch (e) {
     console.error(e);
     res.status(500).json({message: "500: ガチャ失敗"});
