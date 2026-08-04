b27e39449f18247509af236eb08653cf867f0cb7
kajikaji5678 <bestlife.fukuda.kajito@gmail.com>
2026-08-04 18:13:35 +0900

    画面推移完了

--- src/pages/Result.tsx
+++ src/pages/Result.tsx
@@ -1,10 +1,21 @@
 import { useLocation } from "react-router-dom";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import "./Result.css"
 
 export default function Result() {
 
+  const navigate = useNavigate();
   const location = useLocation();
   const card = location.state?.card;
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      navigate("/");
+    }, 4000);
+
+    return() => clearTimeout(timer);
+  }, [navigate]);
 
   return (
     <div className="result-screen flex flex-col">
