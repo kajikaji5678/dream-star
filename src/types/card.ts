19711166c03ab4f6e2563cfc944f704c1ac8a8a9
kajikaji5678 <bestlife.fukuda.kajito@gmail.com>
2026-08-05 01:06:45 +0900

    fix: コンポーネント化

--- src/types/card.ts
+++ src/types/card.ts
@@ -4,6 +4,13 @@ export type CardFormData = {
   rarity: string
 }
 
+export type Card = {
+  id: number;
+  imageUrl: string;
+  name: string;
+  rarity: string;
+}
+
 export type CardInfo = {
   label: string;
   key: keyof CardFormData;
