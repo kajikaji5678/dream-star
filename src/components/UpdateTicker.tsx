b9e0889386705ddd3c757c5d50e20b8a8d6ce40d
kajikaji5678 <bestlife.fukuda.kajito@gmail.com>
2026-08-11 05:39:09 +0900

    v1.0.2

--- src/components/UpdateTicker.tsx
+++ src/components/UpdateTicker.tsx
@@ -12,7 +12,7 @@ export default function UpdateTicker() {
           ease: "linear",
           repeat: Infinity
         }}>
-          v1.0.1 ローディング画面を更新しました
+          v1.0.2 インベントリ画面にてレアリティの色分けおよびNew表示を行いました
       </motion.div>
     </div>
   )
