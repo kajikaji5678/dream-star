989e0d395bb4184e050505952f14e8fe9fc761f6
kajikaji5678 <bestlife.fukuda.kajito@gmail.com>
2026-08-17 18:30:34 +0900

    Tips追加

--- src/pages/Loading.tsx
+++ src/pages/Loading.tsx
@@ -6,7 +6,24 @@ type LoadingProps = {
   msg: string;
 }
 
+const randomLoadingTips = [
+  "DREAMキャラは倒すと2ポイント！",
+  "「前衛」に移動できるキャラでアタッカーを守ろう",
+  "「消費ポイント」は計画的に",
+  "時には「逃げる」にリソースを割り振ろう",
+  "一発逆転！DREAMキャラ！",
+  "フロントエンドの技術持ってる人、開発手伝って欲しいな(´・ω・)",
+  "「貫通ダメージ」は鉄壁シールドの超過防護やダメージ減少バフを無視する",
+  "相手の弱点を突くと＋2ダメージ！",
+  "「貫通ダメージ」は鉄壁シールドの超過防護やダメージ減少バフを無視する",
+  "「追撃」や「SKILL」によるダメージにダメージ増加バフは乗らない"
+]
+
+
+const randomIndex = Math.floor(Math.random() * randomLoadingTips.length);
+
 export default function Loading({ progress, msg }: LoadingProps) {
+
   return (
     <div className="flex h-screen flex-col items-center justify-center bg-neutral-950 text-white">
       <motion.div
@@ -24,7 +41,7 @@ export default function Loading({ progress, msg }: LoadingProps) {
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 1 }}
       >
-        DreamStar
+        DreamStarLight
       </motion.h1>
       <p className="mt-4 text-center text-gray-300">
         {progress}%
@@ -39,6 +56,9 @@ export default function Loading({ progress, msg }: LoadingProps) {
       </div>
       <p className="mt-2 text-center text-gray-300">
         {msg}
+      </p>
+      <p className="mt-2 text-center text-gray-300">
+        {randomLoadingTips[randomIndex]}
       </p>
       <img
         src="/menuCardImages/hitomaz.gif"
