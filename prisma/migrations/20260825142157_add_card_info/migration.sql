5959b7b12956a553bad50c586546ddd90bb53682
kajikaji5678 <bestlife.fukuda.kajito@gmail.com>
2026-08-25 23:22:11 +0900

    マイグレーション更新

---
+++ prisma/migrations/20260825142157_add_card_info/migration.sql
@@ -0,0 +1,9 @@
+-- AlterTable
+ALTER TABLE "Card" ADD COLUMN     "HP" TEXT,
+ADD COLUMN     "SKILL" TEXT,
+ADD COLUMN     "attack" TEXT,
+ADD COLUMN     "category" TEXT,
+ADD COLUMN     "consumePoint" TEXT,
+ADD COLUMN     "escapePoint" TEXT,
+ADD COLUMN     "supportType" TEXT,
+ADD COLUMN     "type" TEXT;
