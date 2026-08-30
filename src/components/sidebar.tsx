b1c86b50d488a6bed6242ee179317ec72d660891
kajikaji5678 <bestlife.fukuda.kajito@gmail.com>
2026-08-30 23:23:46 +0900

    stlye: サイドバーUIにモーション追加

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,4 +1,5 @@
 import { useNavigate } from "react-router-dom";
+import { motion, type Variants } from "framer-motion";
 
 type MenuCardProps = {
   icon: string;
@@ -7,14 +8,55 @@ type MenuCardProps = {
   path: string;
 };
 
+const sidebarVariants: Variants = {
+  hidden: {
+    x: "-100%",
+  },
+
+  visible: {
+    x: 0,
+    transition: {
+      duration: 0.6,
+      ease: "easeOut",
+
+      // asideが完全に入ってから子要素開始
+      when: "beforeChildren",
+
+      // 最初のカードまで少し待つ
+      delayChildren: 0.1,
+      // カードを順番に出す
+      staggerChildren: 0.2,
+    },
+  },
+};
+
+const cardVariants: Variants = {
+  hidden: {
+    opacity: 0,
+    x: -30,
+    rotate: 45
+  },
+
+  visible: {
+    opacity: 1,
+    x: 0,
+    y: 0,
+    rotate: 0,
+    transition: {
+      duration: 0.3,
+      ease: "easeOut",
+    },
+  },
+};
+
 function MenuCard({ icon, title, bgImage, path }: MenuCardProps) {
   const navigate = useNavigate();
 
   return (
     <div className="menu-card shrink-0 rounded-xl
-    relative flex flex-col bg-[#53565e] items-center justify-center" 
-    onClick={() => navigate(path)}>
-      <div className="menu-background" style={{backgroundImage: `url(${bgImage})`}}></div>
+    relative flex flex-col bg-[#53565e] items-center justify-center"
+      onClick={() => navigate(path)}>
+      <div className="menu-background" style={{ backgroundImage: `url(${bgImage})` }}></div>
       <div className="menu-icon text-5xl pt-4">{icon}</div>
       <span className="menu-title mt-2 font-bold text-lg">{title}</span>
       <div className="menu-triangle" />
@@ -53,10 +95,30 @@ export default function Sidebar() {
 
 
   return (
-    <aside className="w-1/3 h-full bg-[#2b2d31] p-6 flex flex-col gap-4 overflow-y-auto">
-      {menues.map((menu) => (
-        <MenuCard key={menu.title} icon={menu.icon} title={menu.title} bgImage={menu.bgImage} path={menu.path}/>
+    <motion.aside
+      className="w-1/3 h-full bg-[#2b2d31] p-6 flex flex-col gap-4 overflow-y-auto"
+      variants={sidebarVariants}
+      initial="hidden"
+      animate="visible"
+    >
+      {menues.map((menu, index) => (
+        <motion.div
+          key={menu.title}
+          variants={cardVariants}
+          style={{
+            "--card-index": index,
+          } as React.CSSProperties}
+        >
+          <div>
+            <MenuCard
+              icon={menu.icon}
+              title={menu.title}
+              bgImage={menu.bgImage}
+              path={menu.path}
+            />
+          </div>
+        </motion.div>
       ))}
-    </aside>
+    </motion.aside>
   )
 }
