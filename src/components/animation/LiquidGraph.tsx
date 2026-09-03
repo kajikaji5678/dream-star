139b951bb5730cae5be7677ccbda826958cab21d
kajikaji5678 <bestlife.fukuda.kajito@gmail.com>
2026-09-03 21:30:52 +0900

    style: データUI基盤

--- src/components/animation/LiquidGraph.tsx
+++ src/components/animation/LiquidGraph.tsx
@@ -17,13 +17,20 @@ export default function LiquidGraph({ value }: Props) {
       series: [
         {
           type: "liquidFill",
-          data: [value / 100],
-          radius: "80%",
+          data: [value / 100, (value / 100) * 0.8],
+          radius: "90%",
           color: ["#38bdf8"],
+          amplitude: 2.5,
           label: {
             formatter: `${value}%`,
             color: "#fff",
-          }
+          },
+          waveAnimation: true,
+          outline: {
+            show: false
+          },
+          animationDuration: 1200,
+          animationDurationUpdate: 1200,
         }
       ]
     });
@@ -31,5 +38,5 @@ export default function LiquidGraph({ value }: Props) {
     return () => chart.dispose();
   }, [value]);
 
-  return <div ref={chartRef} className="h-full w-full"></div>
+  return <div ref={chartRef} className="h-[150px] w-[150px] absolute top-3 left-3"></div>
 }
--- src/pages/data/Data.tsx
+++ src/pages/data/Data.tsx
@@ -1,6 +1,36 @@
 import LiquidGraph from "@/components/animation/LiquidGraph";
 import Layout from "@/layouts/Layout";
 import type { User } from "@/types/ user";
+import { motion } from "framer-motion"
+
+type Rarity = "C" | "SP" | "R" | "DREAM" | "DR" | "GXR";
+
+const rarityStyle: Record<Rarity, string> = {
+  C: "bg-gray-400",
+  SP: "bg-blue-400",
+  R: "bg-purple-400",
+  DREAM: "bg-yellow-400",
+  DR: "bg-pink-400",
+  GXR: "bg-pink-400",
+};
+
+const rarities: Rarity[] = [
+  "C",
+  "SP",
+  "R",
+  "DREAM",
+  "DR",
+  "GXR"
+]
+
+const progress: Record<Rarity, number> = {
+  C: 80,
+  SP: 60,
+  R: 45,
+  DREAM: 30,
+  DR: 20,
+  GXR: 10,
+};
 
 export default function Data({ user }: User) {
   return (
@@ -9,14 +39,27 @@ export default function Data({ user }: User) {
         <div className="self-start px-4 py-2 mb-4 bg-gradient-to-r from-sky-900 to-sky-500 w-full rounded">
           <h1 className="font-bold">ユーザーデータ</h1>
         </div>
-        <div className="flex flex-1 gap-4">
-          <div className="h-full w-1/3 rounded bg-[#313338]">
-            <div className="h-1/2">
-              <LiquidGraph value={80}/>
-            </div>
+        <div className="lg:hidden bg-transparent overflow-y-auto">
+          <div className="relative h-[180px] p-4 bg-[#313338]">
+            <LiquidGraph value={20}></LiquidGraph>
+            <p className="absolute top-[70px] left-[170px]">
+              図鑑達成率は<span className="font-bold">20%</span>です。
+            </p>
           </div>
-          <div className="h-full w-2/3 rounded bg-[#313338]">
-
+          <div className="px-4 py-2 mt-4 bg-[#313338]">
+            {rarities.map((rarity) => (
+              <div key={rarity} className="flex mt-1 items-center gap-2">
+                <span className="w-14 text-sm text-white">
+                  {rarity}
+                </span>
+                <div className="h-[6px] flex-1 overflow-hidden rounded-full bg-gray-700">
+                  <motion.div
+                    className={`h-full rounded-full ${rarityStyle[rarity]}`}
+                    initial={{ width: "0%" }}
+                    animate={{ width: `${progress[rarity]}%` }}
+                    transition={{ duration: 1, ease: "easeInOut" }} />
+                </div>
+              </div>))}
           </div>
         </div>
       </section>
