b06e1f5e6ad4fbeadb8536f5c5be6762540e36ed
kajikaji5678 <bestlife.fukuda.kajito@gmail.com>
2026-09-06 03:54:17 +0900

    なぜかファイル紛失

--- server/app.ts
+++ server/app.ts
@@ -9,6 +9,7 @@ import uploadRouter from "./routes/upload.js";
 import gachaRouter from "./routes/gacha.js";
 import userRouter from "./routes/user.js";
 import loginRouter from "./routes/login.js";
+import RankingRouter from "./routes/ranking.js"
 
 const app = express();
 
@@ -21,6 +22,7 @@ app.use("/api/upload", uploadRouter);
 app.use("/api/gacha", gachaRouter);
 app.use("/api/users", userRouter);
 app.use("/api/login", loginRouter);
+app.use("/api/ranking", RankingRouter);
 
 app.use(
   "/uploads",
