63e1e42f142c5f17a64cabf1273859e472dfc569
kajikaji5678 <bestlife.fukuda.kajito@gmail.com>
2026-09-17 23:27:07 +0900

    コンポーネント移植

--- src/pages/admin/AbilityEditor/AbilityEffect.tsx
+++ src/pages/admin/AbilityEditor/AbilityEffect.tsx
@@ -1,11 +1,8 @@
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import { Select, SelectContent, SelectItem, SelectValue } from "@/components/ui/select";
-import { SelectTrigger } from "@/components/ui/select"
 import { saveEffect } from "@/service/CMSService";
 import { useEffect, useState } from "react";
+import EffectForm from "./EffectForm";
 
 type Props = {
   onBack: () => void;
@@ -31,7 +28,6 @@ const emptyEffect: Effect = {
 export default function AbilityEffect({ onBack, abilityId }: Props) {
 
   const [effectId, setEffectId] = useState<number | null>(null);
-  const [isSpecialOpen, setIsSpecialOpen] = useState(false);
   const [error, setError] = useState("");
   const [success, setSuccess] = useState("");
   const [effects, setEffects] = useState<Effect[]>([emptyEffect]);
@@ -110,109 +106,10 @@ export default function AbilityEffect({ onBack, abilityId }: Props) {
       )}
 
       <CardContent className="mt-2 space-y-6">
-        <div className="space-y-2">
-          <Label className="text-base">効果の種類</Label>
-          <Select
-            value={effects[activeEffect].effectType}
-            onValueChange={(value) => updateEffect("effectType", value ?? "")}>
-            <SelectTrigger className="border-[#1e1f22] bg-[#a4a4a5]">
-              <SelectValue placeholder="種類を選択"></SelectValue>
-            </SelectTrigger>
-            <SelectContent>
-              <SelectItem value="damage">
-                ダメージ
-              </SelectItem>
-              <SelectItem value="heal">
-                回復
-              </SelectItem>
-              <SelectItem value="special">
-                特殊状態
-              </SelectItem>
-              <SelectItem value="draw">
-                ドロー
-              </SelectItem>
-            </SelectContent>
-          </Select>
-        </div>
-
-        {isSpecialOpen && (
-          <div className="space-y-2">
-            <Label className="text-base">特殊効果の内容を指定</Label>
-            <Select
-              value={effects[activeEffect].specialStatus}
-              onValueChange={(value) => updateEffect("specialStatus", value ?? "")}
-            >
-              <SelectTrigger className="border-[#1e1f22] bg-[#a4a4a5]">
-                <SelectValue placeholder="種類を選択"></SelectValue>
-              </SelectTrigger>
-              <SelectContent>
-                <SelectItem value="poison">
-                  毒
-                </SelectItem>
-                <SelectItem value="stan">
-                  スタン
-                </SelectItem>
-                <SelectItem value="ghost">
-                  呪い
-                </SelectItem>
-                <SelectItem value="rock_on">
-                  ロックオン
-                </SelectItem>
-                <SelectItem value="explotion">
-                  爆破
-                </SelectItem>
-                <SelectItem value="not_move">
-                  行動不能
-                </SelectItem>
-              </SelectContent>
-            </Select>
-          </div>
-        )}
-
-        <div className="space-y-2">
-          <Label className="text-base">効果対象</Label>
-          <Select
-            value={effects[activeEffect].target}
-            onValueChange={(value) => updateEffect("target", value ?? "")}>
-            <SelectTrigger className="border-[#1e1f22] bg-[#a4a4a5]">
-              <SelectValue placeholder="対象を選択します"></SelectValue>
-            </SelectTrigger>
-            <SelectContent>
-              <SelectItem value="self">
-                自分
-              </SelectItem>
-              <SelectItem value="ally_only">
-                自分以外の味方一人
-              </SelectItem>
-              <SelectItem value="ally_all">
-                味方全体
-              </SelectItem>
-              <SelectItem value="enemy_self">
-                相手
-              </SelectItem>
-              <SelectItem value="enemy_only">
-                敵の一人
-              </SelectItem>
-              <SelectItem value="enemy_all">
-                敵全体
-              </SelectItem>
-              <SelectItem value="all">
-                全員
-              </SelectItem>
-            </SelectContent>
-          </Select>
-        </div>
-
-        <div className="space-y-2">
-          <Label className="text-base">数値(ダメージ・枚数など)</Label>
-          <Input
-            type="number"
-            placeholder="例: 50"
-            value={effects[activeEffect].valueNumber ?? ""}
-            onChange={(e) => updateEffect("valueNumber", e.target.value)}
-            className="border-[#1e1f22] bg-[#a4a4a5] text-whit"
-          />
-        </div>
+        <EffectForm
+          effect={effects[activeEffect]}
+          onChange={updateEffect}
+        />
         <Button
           variant="outline"
           className="bg-blue-200 hover:bg-blue-400 text-black"
