d5204c8e4d875462fa537eebf988dec92312c2fb
kajikaji5678 <bestlife.fukuda.kajito@gmail.com>
2026-09-19 00:14:42 +0900

    テスト完了

--- src/pages/user/CardView.tsx
+++ src/pages/user/CardView.tsx
@@ -2,9 +2,10 @@ import type { Card } from "@/types/card";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import "./CardView.css";
+import Tilt from "react-parallax-tilt";
 
 export default function CardView() {
-  const id = useParams<{id: string}>();
+  const { id } = useParams<{ id: string }>();
   const [card, setCard] = useState<Card | null>(null);
   const testCard = "/public/menuCardImages/Dr.srone.png";
 
@@ -19,11 +20,25 @@ export default function CardView() {
   if (!card) return <div>Loading</div>
 
   return (
-    <div className="h-full w-full flex items-center justify-center p-6">
-      <img
-        src={testCard}
-        className="detail-card" 
-      />
-    </div>
+    <Tilt
+      tiltMaxAngleY={15}
+      tiltMaxAngleX={15}
+      perspective={800}
+      scale={1.05}
+      transitionSpeed={1000}
+      glareMaxOpacity={0.3}
+    >
+      <div className="h-full w-full flex items-center justify-center p-6">
+        <div
+          className="card-wrapper"
+        >
+          <img
+            src={card.imageUrl}
+            className="detail-card"
+          />
+          <div className="card-shine" />
+        </div>
+      </div>
+    </Tilt>
   )
 }
