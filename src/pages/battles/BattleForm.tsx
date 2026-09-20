75db10fdf41186fe75704e3f29263d0454c4ab05
kajikaji5678 <bestlife.fukuda.kajito@gmail.com>
2026-09-21 08:49:54 +0900

    ドラックドロップ設置

--- src/pages/battles/BattleForm.tsx
+++ src/pages/battles/BattleForm.tsx
@@ -1,6 +1,6 @@
 import "./BattleForm.css"
 import { useEffect, useState } from "react";
-
+import { DndContext, useDraggable } from "@dnd-kit/core";
 export default function BattleForm() {
 
   const sampleCards = [
@@ -26,6 +26,8 @@ export default function BattleForm() {
 
   const selectedCard = hand[hand.length - 1];
 
+  const { attributes, listeners, setNodeRef, transform } = useDraggable({ id: `card-${selectedCard.id}` })
+
   useEffect(() => {
     const handleKeyDown = (event: KeyboardEvent) => {
       if (event.key === "a") {
@@ -50,80 +52,92 @@ export default function BattleForm() {
 
   return (
     <>
-      <div className="battle-screen">
-        <div className="battle-mat">
-          <div className="battle-side opponent">
-            <div className="battle-row">
-              <span className="row-label">前衛</span>
+      <DndContext>
+        <div className="battle-screen">
+          <div className="battle-mat">
+            <div className="battle-side opponent">
+              <div className="battle-row">
+                <span className="row-label">前衛</span>
 
-              <div className="card-slots">
-                {[1, 2, 3, 4].map((slot) => (
-                  <div className="card-slot" key={slot} />
-                ))}
+                <div className="card-slots">
+                  {[1, 2, 3, 4].map((slot) => (
+                    <div className="card-slot" key={slot} />
+                  ))}
+                </div>
+              </div>
+
+              <div className="battle-row">
+                <span className="row-label">中衛</span>
+
+                <div className="card-slots">
+                  {[1, 2, 3, 4].map((slot) => (
+                    <div className="card-slot" key={slot} />
+                  ))}
+                </div>
+              </div>
+
+              <div className="battle-sub">
+                <span className="row-label-back">後衛</span>
+                <div className="card-slot-back" />
               </div>
             </div>
+            <div className="battle-side player">
+              <div className="battle-row">
+                <span className="row-label">前衛</span>
 
-            <div className="battle-row">
-              <span className="row-label">中衛</span>
+                <div className="card-slots">
+                  {[1, 2, 3, 4].map((slot) => (
+                    <div className="card-slot" key={slot} />
+                  ))}
+                </div>
+              </div>
 
-              <div className="card-slots">
-                {[1, 2, 3, 4].map((slot) => (
-                  <div className="card-slot" key={slot} />
-                ))}
+              <div className="battle-row">
+                <span className="row-label">中衛</span>
+
+                <div className="card-slots">
+                  {[1, 2, 3, 4].map((slot) => (
+                    <div className="card-slot" key={slot} />
+                  ))}
+                </div>
+              </div>
+
+              <div className="battle-sub">
+                <span className="row-label-back">後衛</span>
+                <div className="card-slot-back" />
               </div>
             </div>
-
-            <div className="battle-sub">
-              <span className="row-label-back">後衛</span>
-              <div className="card-slot-back" />
+            <div className="my-hand">
+              {hand.map((card, index) => (
+                <img
+                  className="my-hand-card"
+                  key={card.id}
+                  src={card.image}
+                  style={{
+                    zIndex: index,
+                    left: `${index * 25}px`,
+                  }}
+                />
+              ))}
             </div>
           </div>
-          <div className="battle-side player">
-            <div className="battle-row">
-              <span className="row-label">前衛</span>
-
-              <div className="card-slots">
-                {[1, 2, 3, 4].map((slot) => (
-                  <div className="card-slot" key={slot} />
-                ))}
-              </div>
-            </div>
-
-            <div className="battle-row">
-              <span className="row-label">中衛</span>
-
-              <div className="card-slots">
-                {[1, 2, 3, 4].map((slot) => (
-                  <div className="card-slot" key={slot} />
-                ))}
-              </div>
-            </div>
-
-            <div className="battle-sub">
-              <span className="row-label-back">後衛</span>
-              <div className="card-slot-back" />
-            </div>
-          </div>
-          <div className="my-hand">
-            {hand.map((card, index) => (
-              <img
-                className="my-hand-card"
-                key={card.id}
-                src={card.image}
-                style={{
-                  zIndex: index,
-                  left: `${index * 25}px`,
-                }}
-              />
-            ))}
+          <div
+            ref={setNodeRef}
+            className="selected-card"
+            {...listeners}
+            {...attributes}
+            style={{
+              transform: transform
+                ? `translate3d(${transform.x}px, ${transform.y}px, 0)`
+                : undefined
+            }}
+          >
+            <img
+              src={selectedCard.image}
+            />
           </div>
         </div>
-        <div className="selected-card">
-          <img
-            src={selectedCard.image}
-          />
-        </div>
-      </div>
+      </DndContext>
     </>
   )
 }
