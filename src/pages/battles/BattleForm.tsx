5905d5386be3b4686d55d1fe0add68f43d17503d
kajikaji5678 <bestlife.fukuda.kajito@gmail.com>
2026-09-21 05:05:46 +0900

    style: カード基盤

--- src/pages/battles/BattleForm.tsx
+++ src/pages/battles/BattleForm.tsx
@@ -5,32 +5,56 @@ export default function BattleForm() {
     <>
       <div className="battle-screen">
         <div className="battle-mat">
-          {/**前衛 */}
-          <div className="battle-row">
-            <span className="row-label">前衛</span>
-            <div className="card-slots">
-              {[1, 2, 3, 4].map((slot) => (
-                <div className="card-slot" key={slot} />
-              ))}
+          <div className="battle-side opponent">
+            <div className="battle-row">
+              <span className="row-label">前衛</span>
+
+              <div className="card-slots">
+                {[1, 2, 3, 4].map((slot) => (
+                  <div className="card-slot" key={slot} />
+                ))}
+              </div>
+            </div>
+
+            <div className="battle-row">
+              <span className="row-label">中衛</span>
+
+              <div className="card-slots">
+                {[1, 2, 3, 4].map((slot) => (
+                  <div className="card-slot" key={slot} />
+                ))}
+              </div>
+            </div>
+
+            <div className="battle-sub">
+              <span className="row-label-back">後衛</span>
+              <div className="card-slot-back" />
             </div>
           </div>
-          {/**中衛 */}
-          <div className="battle-row">
-            <div className="card-slots">
-              {[1, 2, 3, 4].map((slot) => (
-                <div className="card-slot" key={slot} />
-              ))}
+          <div className="battle-side player">
+            <div className="battle-row">
+              <span className="row-label">前衛</span>
+
+              <div className="card-slots">
+                {[1, 2, 3, 4].map((slot) => (
+                  <div className="card-slot" key={slot} />
+                ))}
+              </div>
             </div>
-          </div>
-          {/**後衛 + 山札 */}
-          <div className="back-row">
-            <div>
-              <span>後衛</span>
-              <div className="card-slot" />
+
+            <div className="battle-row">
+              <span className="row-label">中衛</span>
+
+              <div className="card-slots">
+                {[1, 2, 3, 4].map((slot) => (
+                  <div className="card-slot" key={slot} />
+                ))}
+              </div>
             </div>
-            <div>
-              <span>山札</span>
-              <div className="card-slot" />
+
+            <div className="battle-sub">
+              <span className="row-label-back">後衛</span>
+              <div className="card-slot-back" />
             </div>
           </div>
         </div>
